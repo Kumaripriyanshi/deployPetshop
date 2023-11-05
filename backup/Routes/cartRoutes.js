f5a93a19a570baacad31b0151a273d475598d3e6
priyanshi <kumarmalliksanjay81@gmail.com>
2023-11-05 15:57:03 +0530

    deleted

--- backup/Routes/cartRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { addInCart } from '../controllers/cartController.js';
-
-
-const router = express.Router();
-
-router.post("/addInCart",addInCart);
-
-export default router;
